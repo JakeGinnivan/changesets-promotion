df7cb7e8d949cf6e30f2f57fd7671d22082872d2
JakeGinnivan <jake@ginnivan.net>
2020-06-26 11:27:58 +0800

    Fixed more issues in deploy script

--- app/deploy.js
+++ app/deploy.js
@@ -1,19 +1,22 @@
-const cmd = require('node-cmd')
+const cmd = require("node-cmd");
 
-const runAsync = Promise.promisify(cmd.run, { multiArgs: true, context: cmd })
- 
-export async function getPublishState() {
+const runAsync = Promise.promisify(cmd.run, { multiArgs: true, context: cmd });
+
+module.exports = {
+  async getPublishState() {
     // Cannot get current publish state
-    return
-}
-
-export async function publish(_pkg, tag) {
+    return;
+  },
+  async publish(_pkg, tag) {
     try {
-        await runAsync(`echo "Publishing version ${tag}"to ${process.env.TARGET_ENVIRONMENT}`)
+      await runAsync(
+        `echo "Publishing version ${tag}"to ${process.env.TARGET_ENVIRONMENT}`
+      );
     } catch (err) {
-        console.error('Failed to publish', err)
-        return { published: false }
+      console.error("Failed to publish", err);
+      return { published: false };
     }
 
-    return { published: true }
-}
+    return { published: true };
+  },
+};
